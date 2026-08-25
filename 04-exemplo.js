c1ec5cca5c186d02abf2253dfe82ae6ca879d53d
Otaviorib05 <tataotavio633@gmail.com>
2026-08-25 23:48:59 +0000

    ola

---
+++ 04-exemplo.js
@@ -0,0 +1,13 @@
+let produto = 'arroz';
+
+switch(produto){
+    case 'arroz':
+        console.log('produto: arroz. Preço: R$ 5,00');
+        break;
+        case 'feijao':
+        console.log('produto: feijao. Preço: R$ 8,00');
+        break;
+        default:
+            console.log('Produto nao encontrado');
+
+}
